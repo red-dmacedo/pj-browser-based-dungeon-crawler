3b5d866f717e60dff6a24226875d3be5f6ab3eed
dmacedo <devinm3333@outlook.com>
2025-07-07 12:08:22 -0700

    added some attacks to js/data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,50 @@
-// placeholder
+export const attacks = {
+  fairyLightning: {
+    name:"fairyLightning",
+    atk:15,
+    mp:20,
+    helpText:"Cannot stun; Deals mid-range damage"
+  },
+  fire: {
+    name:"Fire",
+    atk:70,
+    mp:50,
+    helpText:"Highest Atk damage"
+  },
+  hyperSlash: {
+    name:"Hyper Slash",
+    atk:60,
+    mp:0,
+    helpText:"The pinnacle of slashing techniques"
+  },
+  lightning: {
+    name:"Lightning",
+    atk:15,
+    mp:20,
+    helpText:"Chance to stun enemy; Deals mid-range damage"
+  },
+  nudge: {
+    name:"Nudge",
+    atk:5,
+    mp:0,
+    helpText:"Minimum damage"
+  },
+  slash: {
+    name:"Slash",
+    atk:30,
+    mp:0,
+    helpText:"A slash that will get the job done"
+  },
+  strongSlash: {
+    name:"Strong Slash",
+    atk:60,
+    mp:0,
+    helpText:"A dangerous swing with a lot of power"
+  },
+  water: {
+    name:"Water",
+    atk:10,
+    mp:10,
+    helpText:"Low damage; Kills enemies below x% HP"
+  },
+};
